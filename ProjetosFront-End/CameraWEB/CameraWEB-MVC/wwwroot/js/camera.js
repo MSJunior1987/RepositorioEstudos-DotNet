1637dd1e307dc200ea6a35672fa56438066e4776
Marcio Junior de Souza <marcio.souza2@gmail.com>
2021-10-28 21:05:57 -0300

    Incluidas alteracoes de upload de imagem

--- ProjetosFront-End/CameraWEB/CameraWEB-MVC/wwwroot/js/camera.js
+++ ProjetosFront-End/CameraWEB/CameraWEB-MVC/wwwroot/js/camera.js
@@ -1,3 +1,4 @@
+////Metodos de funcionalidade da camera////
 document.querySelector('#btnInserirImagem').addEventListener('click', function (e) {
    $('#divVideo').show();
    $('#divBotoesFotografar').show();
@@ -47,4 +48,48 @@ document.querySelector('#idFechar').addEventListener('click', function (e) {
    stream.getTracks()[0].stop();  
 })   
 
+document.querySelector('#idSalvarFoto').addEventListener('click', function (e) {     
+   var foto = document.querySelector("#imgFoto");
+   foto.src = document.querySelector('#cvFoto').toDataURL("image/png");      
+   $('#divModalCamera').modal('hide')
+}) 
 
+//// Botao de Upload de imagem////
+document.querySelector("#fileUpload").onchange = function (e){
+   //Get count of selected files
+   var countFiles = $(this)[0].files.length;
+
+   var imgPath = $(this)[0].value;
+   var extn = imgPath.substring(imgPath.lastIndexOf('.') + 1).toLowerCase();
+   var imgFoto = $("#imgFoto");
+   imgFoto.empty();
+   
+   // var image_holder = $("#image-holder");
+   // image_holder.empty();
+
+   if (extn == "gif" || extn == "png" || extn == "jpg" || extn == "jpeg") {
+       if (typeof (FileReader) != "undefined") {
+
+           //loop for each file selected for uploaded.
+           for (var i = 0; i < countFiles; i++) {
+
+               var reader = new FileReader();
+               reader.onload = function (e) {
+                   $("<img />", {
+                       "src": e.target.result,
+                           "class": "thumb-image"
+                   }).appendTo(imgFoto);
+               }
+
+               imgFoto.show();
+               reader.readAsDataURL($(this)[0].files[i]);
+           }
+
+       } else {
+           alert("Seu browser não suporta o FileReader.");
+       }
+   } else {
+       alert("Por favor selecione somente imagens.");
+   }
+};
+/////////////////////////////////////////////////////
--- ProjetosFront-End/CameraWEB/CameraWEB-MVC/wwwroot/js/site.js
+++ ProjetosFront-End/CameraWEB/CameraWEB-MVC/wwwroot/js/site.js
@@ -2,12 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-function abrirModal(id) {
-    var dialog = $(id).data('dialog');
-    dialog.open();
-}
-function fecharModal(id) {
-    var dialog = $(id).data('dialog');
-    dialog.close();
-}
